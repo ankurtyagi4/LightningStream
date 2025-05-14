ce13083281bdfd78516110c80f9e17337d54abb3
anKuRtyaGI <ankurtyagi.4@gmail.com>
2025-05-14 15:36:43 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,4 +1,4 @@
 print("Hello, World!")
 
 print("\033]8;;https://snow-flake-cheat-sheet.streamlit.app/\033\\Snowflake Cheat Sheet (by ungifted amature)\033]8;;\033\\")
-print("\033]8;;https://medium.com/@pooja.sahu.dwh/%EF%B8%8F-role-based-access-control-rbac-in-snowflake-how-to-secure-your-data-the-right-way-2052d482f600\033\\\033]8;;\033\\")
+print("\033]8;;https://medium.com/@pooja.sahu.dwh/%EF%B8%8F-role-based-access-control-rbac-in-snowflake-how-to-secure-your-data-the-right-way-2052d482f600\033\\RBAC\033]8;;\033\\")
